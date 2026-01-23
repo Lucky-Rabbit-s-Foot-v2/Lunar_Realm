9937919e881175e0d8ae149d80e330dc76c13591
jbaem <101052774+jbaem@users.noreply.github.com>
2026-01-23 16:58:16 +0900

    Init: 초기 프로젝트 세팅

--- Source/Lunar_Realm/Lunar_Realm.Build.cs
+++ Source/Lunar_Realm/Lunar_Realm.Build.cs
@@ -8,7 +8,16 @@ public class Lunar_Realm : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", "CoreUObject", "Engine",
+            "InputCore", "EnhancedInput",
+            "AIModule",
+            "NavigationSystem",
+            "GameplayTasks", "GameplayTags", "GameplayAbilities",
+            "Slate", "SlateCore", "UMG",
+            "Niagara", "NiagaraCore",
+            "VectorVM",
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
