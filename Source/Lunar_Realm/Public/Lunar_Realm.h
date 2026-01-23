90dc32421708946e007e70ce5806cdd54cd554b3
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-23 17:57:02 +0900

    <feat> User Project Header as PCH

--- Source/Lunar_Realm/Public/Lunar_Realm.h
+++ Source/Lunar_Realm/Public/Lunar_Realm.h
@@ -4,3 +4,31 @@
 
 #include "CoreMinimal.h"
 
+
+// ============================================
+// 공통 헤더(PCH) - 프로젝트 전역에서 사용(Pre-Compile Header)
+// ============================================
+// GAS 
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+
+// AttributeSet 관련
+#include "AttributeSet.h"
+
+// GameplayEffect 관련
+#include "GameplayEffect.h"
+#include "GameplayEffectTypes.h"
+#include "GameplayEffectExtension.h"
+
+// GameplayAbility 관련
+#include "Abilities/GameplayAbility.h"
+
+// GameplayTag 관련
+#include "GameplayTagContainer.h"
+
+// 유틸리티
+#include "AbilitySystemBlueprintLibrary.h"
+
+
+//로그 시스템
+#include "System/LoggingSystem.h"
