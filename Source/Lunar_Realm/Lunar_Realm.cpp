c46c915dd176c9540074f5bb42b4c9de1bea85cc
jbaem <101052774+jbaem@users.noreply.github.com>
2026-01-23 16:13:18 +0900

    Initial commit

---
+++ Source/Lunar_Realm/Lunar_Realm.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Lunar_Realm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lunar_Realm, "Lunar_Realm" );
