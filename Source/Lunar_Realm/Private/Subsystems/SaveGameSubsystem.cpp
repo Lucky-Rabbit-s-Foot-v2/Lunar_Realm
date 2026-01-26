4ec2c48fde619f2d67d09877b06845029c225433
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-26 17:51:24 +0900

    <feat> SaveGameSubsystem 제작 및 구현
    * SaveGame 파일의 로드/저장을 담당하는 중앙 집중식 서브시스템
     *
     * 주요 기능:
     * - 파일 I/O
     * - 현재 선택된 캐릭터/장비 정보 관리

---
+++ Source/Lunar_Realm/Private/Subsystems/SaveGameSubsystem.cpp
@@ -0,0 +1,150 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/SaveGameSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+void USaveGameSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	LR_INFO(TEXT("SaveGameSubsystem Initialized"));
+	LoadGame();
+}
+
+void USaveGameSubsystem::Deinitialize()
+{
+	SaveGame();
+	Super::Deinitialize();
+}
+
+void USaveGameSubsystem::CreateNewSaveGame()
+{
+	//세이브게임 로드가 안될 시 사용하는 더미 데이터
+	CurrentSaveGame = Cast<ULRSaveGame>(UGameplayStatics::CreateSaveGameObject(ULRSaveGame::StaticClass()));
+	
+#if WITH_EDITOR
+	//테스트용 데이터......
+	//도감 보유 캐릭터 정보
+	CurrentSaveGame->OwnedCharacters.Add(10102, FPlayerCharacterInstance(10102, 10)); //리더
+	CurrentSaveGame->OwnedCharacters.Add(10301, FPlayerCharacterInstance(10301, 3)); //파티원 1
+	CurrentSaveGame->OwnedCharacters.Add(10201, FPlayerCharacterInstance(10201, 4)); //파티원 2
+	CurrentSaveGame->OwnedCharacters.Add(10101, FPlayerCharacterInstance(10101, 4)); //파티원 3
+	
+	//도감 보유 장비 정보
+	CurrentSaveGame->OwnedEquipments.Add(20100201, FPlayerEquipmentInstance(20100201, 3)); //리더장비 무기
+	CurrentSaveGame->OwnedEquipments.Add(21150102, FPlayerEquipmentInstance(21150102, 3)); //리더장비 헬멧
+	CurrentSaveGame->OwnedEquipments.Add(21255101, FPlayerEquipmentInstance(21255101, 3)); //리더장비 갑옷
+	
+	//선정 파티 정보
+	CurrentSaveGame->SelectedCharacterIDs.Add(10102);
+	CurrentSaveGame->SelectedCharacterIDs.Add(10301);
+	CurrentSaveGame->SelectedCharacterIDs.Add(10201);
+	CurrentSaveGame->SelectedCharacterIDs.Add(10301);
+	
+	//리더 장비 정보
+	CurrentSaveGame->SelectedEquipmentIDs.Add(20100201);
+	CurrentSaveGame->SelectedEquipmentIDs.Add(21150102);
+	CurrentSaveGame->SelectedEquipmentIDs.Add(21255102);
+	
+#endif
+}
+
+void USaveGameSubsystem::LoadGame()
+{
+	if (UGameplayStatics::DoesSaveGameExist(SaveSlotName, UserIndex))
+	{
+		CurrentSaveGame = Cast<ULRSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlotName, UserIndex));
+		if (!ensureMsgf(CurrentSaveGame, TEXT("Could not load GameSaveSlot")))
+		{
+			return;
+		}
+		
+		//브로드캐스트
+		OnSaveGameLoaded.Broadcast(CurrentSaveGame);
+	}
+	else
+	{
+		LR_INFO(TEXT("No Save Game found, creating new one"));
+		CreateNewSaveGame();
+	}
+}
+
+void USaveGameSubsystem::SaveGame()
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CANNOT SAVE : CurrentSaveGame is NULL")))
+	{
+		return;
+	}
+	
+	if (UGameplayStatics::SaveGameToSlot(CurrentSaveGame, SaveSlotName, UserIndex))
+	{
+		LR_INFO(TEXT("Save Game Successfully"));
+		OnSaveGameSaved.Broadcast();
+	}
+}
+
+void USaveGameSubsystem::SetSelectedCharacterSlot(int32 Slots, int32 CharacterID)
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return;
+	}
+	
+	CurrentSaveGame->SetCharacterSlot(Slots, CharacterID);
+	SaveGame(); //자동저장
+	
+	LR_INFO(TEXT("Character Slot %d set to ID %d"), Slots, CharacterID);
+}
+
+int32 USaveGameSubsystem::GetSelectedCharacterSlot(int32 CharacterID) const
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return -1;
+	}
+	
+	return CurrentSaveGame->GetCharacterSlot(CharacterID);
+}
+
+TArray<int32> USaveGameSubsystem::GetAllSelectedCharacters() const
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return TArray<int32>();
+	}
+	
+	return CurrentSaveGame->GetAllCharacterSlots();
+}
+
+void USaveGameSubsystem::SetSelectedEquipmentSlot(int32 Slots, int32 EquipmentIDs)
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return;
+	}
+	
+	CurrentSaveGame->SetEquipmentSlot(Slots, EquipmentIDs);
+	SaveGame(); //자동저장
+	
+	LR_INFO(TEXT("Character Slot %d set to ID %d"), Slots, EquipmentIDs);
+}
+
+int32 USaveGameSubsystem::GetSelectedEquipmentSlot(int32 SlotIdx) const
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return -1;
+	}
+	
+	return CurrentSaveGame->GetEquipmentSlot(SlotIdx);
+}
+
+TArray<int32> USaveGameSubsystem::GetAllSelectedEquipmentIDs() const
+{
+	if (!ensureMsgf(CurrentSaveGame, TEXT("CurrentSaveGame is NULL")))
+	{
+		return TArray<int32>();
+	}
+	
+	return CurrentSaveGame->GetAllEquippedIDs();
+}
---
+++ Source/Lunar_Realm/Public/Subsystems/SaveGameSubsystem.h
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SaveGame/LRSaveGame.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SaveGameSubsystem.generated.h"
+
+/**
+ * SaveGame 파일의 로드/저장을 담당하는 중앙 집중식 서브시스템
+ * 
+ * 주요 기능:
+ * - 파일 I/O 
+ * - 현재 선택된 캐릭터/장비 정보 관리
+ */
+//=============================================================================
+// (260126) KHS 제작. 제반 사항 구현.
+// =============================================================================
+
+//SaveGame 저장/로드 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSaveGameLoaded, ULRSaveGame*, LoadedSave);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSaveGameSaved);
+
+
+UCLASS()
+class LUNAR_REALM_API USaveGameSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+	//델리게이트
+	UPROPERTY(BlueprintAssignable, Category = "LR|SaveGame")
+	FOnSaveGameLoaded OnSaveGameLoaded;
+	
+	UPROPERTY(BlueprintAssignable, Category = "LR|SaveGame")
+	FOnSaveGameSaved OnSaveGameSaved;
+	
+private:
+	void CreateNewSaveGame();
+	
+public:
+	//핵심 기능
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	void LoadGame();
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	void SaveGame();
+	
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	ULRSaveGame* GetCurrentSaveGame() const {return CurrentSaveGame;}
+	
+	//세이브 캐릭터/장비 정보 관리
+	//선택된 캐릭터 파티 정보 관리
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	void SetSelectedCharacterSlot(int32 Slots, int32 CharacterID);
+	
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	int32 GetSelectedCharacterSlot(int32 CharacterID) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	TArray<int32> GetAllSelectedCharacters() const;
+	
+	//선택된 리더 캐릭터 장비 정보 관리
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	void SetSelectedEquipmentSlot(int32 Slots, int32 EquipmentIDs);
+	
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	int32 GetSelectedEquipmentSlot(int32 SlotIdx) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "LR|SaveGame")
+	TArray<int32> GetAllSelectedEquipmentIDs() const;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<ULRSaveGame> CurrentSaveGame;
+	
+	const FString SaveSlotName = TEXT("PlayerSaveSlot");
+	const uint32 UserIndex = 0;
+};
