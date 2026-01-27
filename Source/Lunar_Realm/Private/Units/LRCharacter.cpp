f01a421d14bd8dffbe52ea83975828d5574ec49c
jbaem <101052774+jbaem@users.noreply.github.com>
2026-01-27 11:12:19 +0900

    Init: 프로젝트 베이스 캐릭터 생성

---
+++ Source/Lunar_Realm/Private/Units/LRCharacter.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Units/LRCharacter.h"
+
+ALRCharacter::ALRCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ALRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Lunar_Realm/Public/Units/LRCharacter.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LRCharacter.generated.h"
+
+/**
+ * LRCharacter 구성 요소
+ * 모든 캐릭터의 베이스 클래스
+ * - 세이브 데이터 관리
+ * - 오브젝트 풀링 처리
+ */
+//============================================================================
+// (260127) PJB 제작. 제반 사항 구현.
+//============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ALRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ALRCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void InitializeByData() {};
+	virtual void SaveData() {};
+	virtual void LoadData() {};
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
