ff496f13b327eca1dcbfa8f075b2135ee7648a0e
jbaem <101052774+jbaem@users.noreply.github.com>
2026-02-03 11:06:53 +0900

    <init> 플로팅 데미지 액터 추가

---
+++ Source/Lunar_Realm/Private/FloatingDamage/LRFloatingDamage.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingDamage/LRFloatingDamage.h"
+
+ALRFloatingDamage::ALRFloatingDamage()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ALRFloatingDamage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALRFloatingDamage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALRFloatingDamage::OnPoolActivate_Implementation()
+{
+	// TODO: 인터페이스 참고
+}
+
+void ALRFloatingDamage::OnPoolDeactivate_Implementation()
+{
+	// TODO: 인터페이스 참고
+}
