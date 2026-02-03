3bbc489a829cb8442f2421eae4a2848218a224aa
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-02-03 09:49:49 +0900

    <feat> player character 및 controller 생성

---
+++ Source/Lunar_Realm/Public/Units/Player/LRPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Units/LRCharacter.h"
+#include "LRPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ALRPlayerCharacter : public ALRCharacter
+{
+	GENERATED_BODY()
+	
+};
