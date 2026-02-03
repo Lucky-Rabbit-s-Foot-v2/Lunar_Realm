1e2b9ed40b673d4899049200a2f33cfa0b53b3e7
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-02-03 13:53:26 +0900

    <feat> player 어트리뷰트셋 / 매크로 헤더 생성

---
+++ Source/Lunar_Realm/Public/Units/Player/GAS/PlayerAttributeSet.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "GameAbilitySystemMacros.h"
+#include "PlayerAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API UPlayerAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
