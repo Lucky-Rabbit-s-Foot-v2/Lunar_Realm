fe9fda1551a4eff23ce7e0acc3a423d7d5e02836
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-02-04 13:45:13 +0900

    <inti> 기지(Core) 부모 클래스 기본 틀 생성 및 구현
    - init : 적 어트리뷰트 생성
    - init : 적 AI 컨트롤러 생성
    - init : 적 부모 클래스 생성
    - chore : 테스트용 기본 적 메시 애니메이션 생성

---
+++ Source/Lunar_Realm/Public/Structures/Core/LRCore.h
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayTagAssetInterface.h"
+#include "GameplayTagContainer.h"
+#include "LRCore.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+/**
+ * LRCore 구성 요소
+ * 각 진영 코어의 베이스 클래스
+ * - 콜리전
+ * - 메쉬
+ * - 체력
+ */
+ //============================================================================
+ // (260204) KWB 제작. 현재 기본틀만 구성
+ //============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ALRCore : public AActor, public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALRCore();
+
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override
+	{
+		TagContainer = OwnedTags;
+	}
+
+	FORCEINLINE UBoxComponent* GetHitCollision() const { return HitCollision; }
+	FORCEINLINE UStaticMeshComponent* GetVisualMesh() const { return VisualMesh; }
+
+	// TEMP
+	int32 GetHealth() { return Health; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHitCollisionBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UBoxComponent> HitCollision = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> VisualMesh = nullptr;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Tags")
+	FGameplayTagContainer OwnedTags;
+
+	// TEMP
+	UPROPERTY(EditAnywhere, Category = "TEMP")
+	int32 Health = 100;
+};
--- Source/Lunar_Realm/Public/Units/Enemy/LREnemyCharacter.h
+++ Source/Lunar_Realm/Public/Units/Enemy/LREnemyCharacter.h
@@ -1,17 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Units/LRCharacter.h"
+#include "Units/Enemy/LREnemyBaseCharacter.h"
 #include "LREnemyCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class LUNAR_REALM_API ALREnemyCharacter : public ALRCharacter
+class LUNAR_REALM_API ALREnemyCharacter : public ALREnemyBaseCharacter
 {
 	GENERATED_BODY()
+	
+
+private:
 	
 };
