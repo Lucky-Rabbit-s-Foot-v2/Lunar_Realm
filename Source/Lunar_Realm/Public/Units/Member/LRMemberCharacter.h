8c5bd9c10f30a7ec36b853444c0d659a8acf1e52
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-02-04 11:58:05 +0900

    <feat> MemberCharacter 생성

---
+++ Source/Lunar_Realm/Public/Units/Member/LRMemberCharacter.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Units/LRCharacter.h"
+#include "AbilitySystemInterface.h"
+#include "GAS/Attributes/LRPlayerAttributeSet.h"
+#include "LRMemberCharacter.generated.h"
+
+
+class UAbilitySystemComponent;
+class ULRPlayerAttributeSet;
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ALRMemberCharacter : public ALRCharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+public:
+	ALRMemberCharacter();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	ULRPlayerAttributeSet* GetAttributeSet() const { return MemberAttributeSet; }
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<ULRPlayerAttributeSet> MemberAttributeSet;
+
+};
