37b71c2ed181fada075d96aa0336f9c58c3c99a5
jbaem <101052774+jbaem@users.noreply.github.com>
2026-02-06 14:25:01 +0900

    <feat> 로딩 위젯 구현

---
+++ Source/Lunar_Realm/Private/UI/Intro/LRLoadingWidget.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Intro/LRLoadingWidget.h"
+
+#include "Components/ProgressBar.h"
+#include "TimerManager.h"
+
+ULRLoadingWidget::ULRLoadingWidget()
+{
+	UILayer = EUILayer::POPUP;
+	ZOrder = 1000;
+}
+
+void ULRLoadingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Progress = 0.f;
+	ElapsedTime = 0.f;
+
+	if (Bar_Loading)
+	{
+		Bar_Loading->SetPercent(Progress);
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(
+		LoadingTimerHandle, 
+		this, 
+		&ULRLoadingWidget::UpdateProgressBar, 
+		0.01f, 
+		true
+	);
+}
+
+void ULRLoadingWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void ULRLoadingWidget::UpdateProgressBar()
+{
+	if (Bar_Loading)
+	{
+		float RemainingTime = TotalDuration - ElapsedTime;
+		float RandomFactor = FMath::FRandRange(0.8f, 1.2f);
+		float Increment = (RandomFactor * 0.01f) / TotalDuration;
+
+		Progress += Increment;
+		ElapsedTime += 0.01f;
+
+		if (ElapsedTime > TotalDuration)
+		{
+			Progress = 1.f;
+			GetWorld()->GetTimerManager().ClearTimer(LoadingTimerHandle);
+		}
+
+		Bar_Loading->SetPercent(Progress);
+	}
+}
---
+++ Source/Lunar_Realm/Public/UI/Intro/LRLoadingWidget.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseWidget.h"
+#include "LRLoadingWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRLoadingWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	ULRLoadingWidget();
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+private:
+	void UpdateProgressBar();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UProgressBar> Bar_Loading = nullptr;
+
+private:
+	FTimerHandle LoadingTimerHandle;
+
+	float Progress = 0.f;
+	float ElapsedTime = 0.f;
+	const float TotalDuration = 7.f;
+};
