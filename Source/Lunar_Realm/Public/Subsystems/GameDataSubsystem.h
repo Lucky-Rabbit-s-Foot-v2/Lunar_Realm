d9e2541d5b3d80b770cddf58f92d1bd93678915a
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-02-09 02:56:45 +0900

    <feat> EnemySpawer 기능 구현
    - <feat> GameDataSubsystem 스테이지 데이터 관련 항목 임시 추가
    - <feat> LREnemyCharacter 오브젝트 풀링 시스템 이용한 스폰 디스폰 기능 추가
    - <feat> LRGameInstance 임시 Stage 관린 주체로 설정 및 필요 필드 작성
    - <feat> LRDataStruct "FStageStaticData" 항목 추가
    - <feat> LRGameDataConfig 스테이지 정적 데이터 테이블 추가

--- Source/Lunar_Realm/Public/Subsystems/GameDataSubsystem.h
+++ Source/Lunar_Realm/Public/Subsystems/GameDataSubsystem.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -24,6 +24,7 @@
 // (260128) KHS 내부 헬퍼가 많아 인터페이스 순서를 public->protected->private순으로 변경
 // (260205) KHS 에너미 데이터 처리 핸들러 추가. ID파싱-> 구조체 생성자로 이전.
 // (260206) KHS 데이터 테이블 소프트 레퍼런스들은 LRGameDataConfig통해 비동기 로드방식으로 변경
+// (260208) KWB 스테이지 데이터 관련 항목 임시 추가 : EnemySpawner 테스트용
 // =============================================================================
 
 UCLASS()
@@ -97,6 +98,12 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "LR|GameData|Enemy")
 	TArray<int32> GetAllEnemyIDs();
 	
+	// ========================================
+	// 스테이지 데이터 조회
+	// ========================================
+	UFUNCTION(BlueprintCallable, Category = "LR|GameData|Stage")
+	const FStageStaticData& GetStageStaticData(int32 StageID) const;
+
 private:
 	// ========================================
 	// 내부 헬퍼 함수
@@ -181,6 +188,8 @@ private:
 	UDataTable* LoadedSkillStaticData;
 	UPROPERTY()
 	UDataTable* LoadedEnemyStaticData;
+	UPROPERTY()
+	UDataTable* LoadedStageStaticData;
 	
 	// ========================================
 	// 데이터 캐싱 (성능 최적화)
@@ -204,6 +213,8 @@ private:
 	TMap<int32, FSkillStaticData> CachedSkillStaticData;
 	//에너미 정적 데이터 캐시
 	TMap<int32, FEnemyStaticData> CachedEnemyStaticData;
+	//스테이지 정적 데이터 캐시
+	TMap<int32, FStageStaticData> CachedStageStaticData;
 	
 	//캐싱 실패시 사용할 기본값
 	static FCharacterStaticData EmptyCharacterStaticData;
@@ -213,6 +224,7 @@ private:
 	static FSetEffectData EmptySetEffectData;
 	static FSkillStaticData EmptySkillStaticData;
 	static FEnemyStaticData EmptyEnemyStaticData;
+	static FStageStaticData EmptyStageStaticData;
 	
 	
 };
@@ -254,6 +266,7 @@ void UGameDataSubsystem::CacheDataTable(UDataTable* DataTable, TMap<E, T>& OutRe
 	if (!DataTable)
 	{
 		LR_WARN(TEXT("DataTable is null"));
+		return;
 	}
 	//기존 데이터 초기화
 	OutRef.Empty();
