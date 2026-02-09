f862c16e150273e065c7c30c88ec3cac00f35a7b
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-02-09 15:36:36 +0900

    <feat> GA_Attack 생성 및 전투 로직 고도화

---
+++ Source/Lunar_Realm/Private/GAS/Ability/Player/LRGA_BasicAttack.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Ability/Player/LRGA_BasicAttack.h"
+#include "Units/Player/Component/LRCombatComponent.h"
+#include "Units/LRCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+
+ULRGA_BasicAttack::ULRGA_BasicAttack()
+{
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Combat.BasicShoot")));
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void ULRGA_BasicAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	ALRCharacter* OwnerChar = Cast<ALRCharacter>(ActorInfo->AvatarActor.Get());
+	if (!OwnerChar)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	AActor* TargetActor = nullptr;
+	ULRCombatComponent* CombatComp = OwnerChar->FindComponentByClass<ULRCombatComponent>();
+	if (CombatComp)
+	{
+		TargetActor = CombatComp->GetCurrentTarget();
+	}
+
+	if (!TargetActor)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (DamageEffectClass && GetAbilitySystemComponentFromActorInfo())
+	{
+		FGameplayEffectContextHandle Context = GetAbilitySystemComponentFromActorInfo()->MakeEffectContext();
+		FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponentFromActorInfo()->MakeOutgoingSpec(DamageEffectClass, 1.0f, Context);
+
+		if (SpecHandle.IsValid())
+		{
+			UAbilitySystemComponent* TargetASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(TargetActor);
+			if (TargetASC)
+			{
+				TargetASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+			}
+		}
+	}
+
+	if (AttackMontage)
+	{
+		OwnerChar->PlayAnimMontage(AttackMontage);
+	}
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
---
+++ Source/Lunar_Realm/Public/GAS/Ability/Player/LRGA_BasicAttack.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/LRGameplayAbilityBase.h"
+#include "LRGA_BasicAttack.generated.h"
+
+/**
+ *  기본 평타 공격 어빌리티
+ */
+
+//=============================================================================
+// (260209) BJM 제작. Attack Ability 생성.
+//=============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ULRGA_BasicAttack : public ULRGameplayAbilityBase
+{
+	GENERATED_BODY()
+	
+public:
+	ULRGA_BasicAttack();
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat")
+	TObjectPtr<UAnimMontage> AttackMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+
+};
