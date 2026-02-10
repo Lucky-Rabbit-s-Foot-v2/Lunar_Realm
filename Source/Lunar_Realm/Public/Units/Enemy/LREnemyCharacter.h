2a1f9f93afaca324db8abca7df05ad13e8a176b2
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-02-11 00:56:43 +0900

    <feat> 에너미 스포너 주석 정리 및 유효성 검증 부분 리팩터
    <feat> 에너미 클래스 비주얼 데이터 초기화 로직 구현, 오브젝트 풀 라이프사이클
    <chore> 프로젝트 맵&모드 게임 인스턴스 수정(임시)
    <docs> 에너미 및 스테이지 데이터 테이블, 값 변경 (테스트용)

--- Source/Lunar_Realm/Public/Units/Enemy/LREnemyCharacter.h
+++ Source/Lunar_Realm/Public/Units/Enemy/LREnemyCharacter.h
@@ -14,12 +14,15 @@ class UGameplayAbility;
 /**
  * 적 캐릭터(Enemy) 베이스 클래스
  * - Attributes 초기화
+ * - 비주얼 데이터 적용 (SkeletalMesh, AnimBP, Scale)
+ * - 오브젝트 풀링 라이프사이클 구현
  */
  //============================================================================
  // (260204) KWB 제작. 제반 사항 구현.
  // (260205) KWB DataSubsystem 구조체에서 Attribute 별 값 받아와 초기화하는 로직 구현
  // (260209) KWB "GrantEnemyAbilities()" 함수 리팩터 -> "FEnemyStaticData" 스킬 항목 "TArray<int32>" 타입으로 캐릭터, 장비와 통일
  // (260210) KWB 키값 타입 int32 -> FName 으로 변경 반영
+ // (260210) KWB 비주얼 데이터 적용, 풀링 라이프 사이클 구현
  //============================================================================
 
 UCLASS()
@@ -44,11 +47,16 @@ public:
 
 	void ClearGrantedEnemyAbilities();
 
+	virtual void OnPoolActivate_Implementation() override;
+	virtual void OnPoolDeactivate_Implementation() override;
+
 protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
 	void InitializeAttributes(FName EnemyID);
+
+	void ApplyVisualData(const struct FEnemyStaticData& EnemyData);
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "LR|ASC")
