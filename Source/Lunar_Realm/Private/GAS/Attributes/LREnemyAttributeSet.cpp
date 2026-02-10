4881f01853b7840eeecf51f27696b54b0c239750
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-02-10 16:27:17 +0900

    <feat> LREnemyAttributeSet 속성 별 값 Clamp 기능 추가
    - <docs> 공통 어트비뷰트셋 헤더에 정보 추가
    - <refactor> LREnemyAttributeSet 일부 속성 값 비율에서 값으로 변경 (ex. 2.0 -> 300.0)

--- Source/Lunar_Realm/Private/GAS/Attributes/LREnemyAttributeSet.cpp
+++ Source/Lunar_Realm/Private/GAS/Attributes/LREnemyAttributeSet.cpp
@@ -2,10 +2,14 @@
 
 
 #include "GAS/Attributes/LREnemyAttributeSet.h"
+#include "System/LoggingSystem.h"
 
 ULREnemyAttributeSet::ULREnemyAttributeSet()
 {
-	// Properties Init Val
+	// TEMP : 추후 실제 값으로 변경 필요
+	InitHealth(10.0f);
+	InitAttack(20.0f);
+	InitSpeed(150.0f);
 }
 
 void ULREnemyAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
@@ -14,6 +18,27 @@ void ULREnemyAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribut
 
 	if (Attribute == GetHealthAttribute())
 	{
-		//NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+		NewValue = FMath::Clamp(NewValue, 0.0f, MaxHealth);
+	}
+
+	if (Attribute == GetAttackAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, MaxAttack);
+	}
+
+	if (Attribute == GetSpeedAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, MaxSpeed);
+	}
+}
+
+void ULREnemyAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		if (GetHealth() <= 0.0f)
+		{
+			// LR_DEBUG(TEXT("%s "));
+		}
 	}
 }
