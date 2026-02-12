57b08e3c18014e3ffb0ad6e52a52d9553c9dfee4
jbaem <101052774+jbaem@users.noreply.github.com>
2026-02-12 13:45:22 +0900

    <init> 로비 UI 클래스 생성

---
+++ Source/Lunar_Realm/Public/UI/OutGame/LRLobbyWidget.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseWidget.h"
+#include "LRLobbyWidget.generated.h"
+
+// =============================================================================
+/**
+ * 로비 UI 위젯
+ */
+ //=============================================================================
+ // (260212) PJB 제작.
+ //=============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ULRLobbyWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+};
