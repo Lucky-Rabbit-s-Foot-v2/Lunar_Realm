50bea928ab81138851f082c2a8a5787e14872886
jbaem <101052774+jbaem@users.noreply.github.com>
2026-02-13 11:09:26 +0900

    <feat> 비어 있는 상점 페이지 추가

---
+++ Source/Lunar_Realm/Public/UI/OutGame/LRShopWidget.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseWidget.h"
+#include "LRShopWidget.generated.h"
+
+
+//============================================================================
+/**
+ * 상점 페이지 UI 위젯
+ * - 기본적으로 존재하는 UI는 없으며, 추후 기획에 따라 구현 예정
+ */
+ //============================================================================
+ // (260213) PJB 제작.
+ //============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ULRShopWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	ULRShopWidget()
+	{
+		UILayer = EUILayer::POPUP;
+	}
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
+private:
+	UFUNCTION(BlueprintCallable, Category = "LR|UI")
+	void CloseButtonClicked();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UButton> Btn_Close;
+};
