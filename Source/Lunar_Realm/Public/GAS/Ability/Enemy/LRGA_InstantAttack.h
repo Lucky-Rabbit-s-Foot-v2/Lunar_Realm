9ae20a161def8e3133bec0c243f767d0b8ccc645
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-02-13 16:55:45 +0900

    <init> LRGA_InstantAttack 추가(에너미 임시 공격)
    <feat> 에너미 캐릭터 OnDie() 기능 구현

---
+++ Source/Lunar_Realm/Public/GAS/Ability/Enemy/LRGA_InstantAttack.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+// TEMP
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/LRGameplayAbilityBase.h"
+#include "LRGA_InstantAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRGA_InstantAttack : public ULRGameplayAbilityBase
+{
+	GENERATED_BODY()
+
+public:
+	ULRGA_InstantAttack();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Range")
+	float AttackRange = 200.0f; // 공격 가능 거리
+};
