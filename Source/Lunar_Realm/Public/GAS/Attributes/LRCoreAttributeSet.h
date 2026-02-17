744a3f31bee361de02d1715db0fdf7773f7170d0
jackbongmin <rrrgk12@gmail.com>
2026-02-17 12:29:44 +0900

    <feat> CoreAttributeSet 생성

---
+++ Source/Lunar_Realm/Public/GAS/Attributes/LRCoreAttributeSet.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Attributes/LRAttributeSet.h"
+#include "GAS/Common/LRGameAbilitySystemMacros.h"
+#include "AbilitySystemComponent.h"
+
+#include "LRCoreAttributeSet.generated.h"
+
+/**
+ * 
+ */
+
+ //=============================================================================
+ // (260217) BJM 제작. 코어 전용 어트리뷰트셋
+ // =============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ULRCoreAttributeSet : public ULRAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	ULRCoreAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(ULRCoreAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(ULRCoreAttributeSet, MaxHealth);
+
+};
