cae14fd4f45f0d5a80c5753385ba46f07e44b95e
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-02-19 15:10:07 +0900

    <fix> GA 트리거 사용방식 수정_ GameplayEvent방식
    GameplayEventData에 Instigator / Target 정보 담아서 GA 발동시키는 방식으로 수정
    - 수정 대상 : GA 서브클래스, GA호출부(AIController / CombatComp)

--- Source/Lunar_Realm/Private/GAS/Ability/Player/LRGA_Charge.cpp
+++ Source/Lunar_Realm/Private/GAS/Ability/Player/LRGA_Charge.cpp
@@ -3,21 +3,31 @@
 
 #include "GAS/Ability/Player/LRGA_Charge.h"
 #include "AbilitySystemComponent.h"
+#include "GAS/Tags/LRGameplayTags.h"
 
 ULRGA_Charge::ULRGA_Charge()
 {
 	//AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Aether.Charge")));
 
-	FGameplayTagContainer Tags;
-	Tags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Aether.Charge")));
-	SetAssetTags(Tags);
+	// FGameplayTagContainer Tags;
+	// Tags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Aether.Charge")));
+	// SetAssetTags(Tags);
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	
+	//(260219) KHS 이벤트 태그를 전달하여 발동되도록 트리거 등록
+	AbilityTags.AddTag(LRTags::Ability_Aether_Charge);
+	
+	FAbilityTriggerData TriggerData;
+	TriggerData.TriggerTag = LRTags::Ability_Aether_Charge;
+	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
+	AbilityTriggers.Add(TriggerData);
 }
 
-void ULRGA_Charge::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
-{
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
+void ULRGA_Charge::OnAbilityActivated(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
