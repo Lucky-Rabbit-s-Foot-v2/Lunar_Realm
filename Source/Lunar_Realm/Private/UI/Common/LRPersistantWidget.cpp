2c1f1e74aaa54a0775eb2de3b8db9fac9d4036aa
jbaem <101052774+jbaem@users.noreply.github.com>
2026-02-19 12:45:35 +0900

    <init> UI 세션화를 위한 Persistant Widget 생성

---
+++ Source/Lunar_Realm/Private/UI/Common/LRPersistantWidget.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Common/LRPersistantWidget.h"
+
+void ULRPersistantWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void ULRPersistantWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void ULRPersistantWidget::OpenUI()
+{
+	Super::OpenUI();
+}
+
+void ULRPersistantWidget::CloseUI()
+{
+	Super::CloseUI();
+}
+
+void ULRPersistantWidget::RefreshUI()
+{
+	Super::RefreshUI();
+}
---
+++ Source/Lunar_Realm/Public/UI/Common/LRPersistantWidget.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseWidget.h"
+#include "LRPersistantWidget.generated.h"
+
+
+//============================================================================
+/**
+ * UI Layer 의 배경이 될 위젯
+ */
+ //============================================================================
+ // (260219) PJB 제작. 제반 사항 구현
+ //============================================================================
+
+UCLASS()
+class LUNAR_REALM_API ULRPersistantWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	ULRPersistantWidget()
+	{
+		UILayer = EUILayer::PERSISTENT;
+	}
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
+	virtual void OpenUI() override;
+	virtual void CloseUI() override;
+	virtual void RefreshUI() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "LR|UI|Persistant")
+	TSubclassOf<UBaseWidget> DefaultPopupClass;
+};
