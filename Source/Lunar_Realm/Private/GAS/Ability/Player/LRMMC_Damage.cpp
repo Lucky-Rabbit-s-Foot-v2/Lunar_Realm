07977e35e8dea5a58f9151772fad4a05469f7ff1
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-03 13:49:29 +0900

    <feat> 공격 수치 랜덤 적용

---
+++ Source/Lunar_Realm/Private/GAS/Ability/Player/LRMMC_Damage.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Ability/Player/LRMMC_Damage.h"
+#include "GAS/Attributes/LRPlayerAttributeSet.h"
+
+
+ULRMMC_Damage::ULRMMC_Damage()
+{
+	// Source(공격자)의 AttackPower를 캡처하겠다고 세팅
+	AttackPowerDef = FGameplayEffectAttributeCaptureDefinition(ULRPlayerAttributeSet::GetAttackPowerAttribute(), EGameplayEffectAttributeCaptureSource::Source, true);
+
+	// 캡처 목록에 등록
+	RelevantAttributesToCapture.Add(AttackPowerDef);
+}
+
+float ULRMMC_Damage::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& InSpec) const
+{
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = InSpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluationParameters.TargetTags = InSpec.CapturedTargetTags.GetAggregatedTags();
+
+	float AttackPower = 0.0f;
+	GetCapturedAttributeMagnitude(AttackPowerDef, InSpec, EvaluationParameters, AttackPower);
+
+	// 오차범위 적용 로직
+	float MinDamage = AttackPower * 0.85f;
+	float MaxDamage = AttackPower * 1.15f;
+
+	return FMath::RandRange(MinDamage, MaxDamage);
+}
---
+++ Source/Lunar_Realm/Public/GAS/Ability/Player/LRMMC_Damage.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "LRMMC_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRMMC_Damage : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	ULRMMC_Damage();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& InSpec) const override;
+
+private:
+	// 공격력을 잡아올 캡처 정의
+	FGameplayEffectAttributeCaptureDefinition AttackPowerDef;
+};
