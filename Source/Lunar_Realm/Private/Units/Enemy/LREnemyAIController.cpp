12ee1145ac3fef442a88ed553f9e4ddabeda3fbc
Rabbit.K <66372336+Rabbit-M13@users.noreply.github.com>
2026-03-03 17:14:03 +0900

    <refactor> LRAIController : 멤버, 에너미 각기 다른 스킬 사용을 위한 TryAttackTarget() 가상함수화
    <feat> LREnemyAIController : TryAttackTarget() 가상함수 구현
    <test> 원할한 테스트를 위한, 에너미 임시 GA 설정
    <chore> LRGA_InstantAttack : 원할한 테스트를 위한 공격 사거리 증가

--- Source/Lunar_Realm/Private/Units/Enemy/LREnemyAIController.cpp
+++ Source/Lunar_Realm/Private/Units/Enemy/LREnemyAIController.cpp
@@ -1,13 +1,47 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Units/Enemy/LREnemyAIController.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "GAS/Tags/LRGameplayTags.h"
+#include "Units/Enemy/LREnemyAIController.h"
+#include "Units/LRCharacter.h"
 
 
 ALREnemyAIController::ALREnemyAIController()
 {
 	HostileRootTag = LRTags::Team_Player;
 	TargetCoreTag = LRTags::Team_Player_Structure_Core;
+}
+
+bool ALREnemyAIController::TryAttackTarget(AActor* Target)
+{
+	if (!Target)
+	{
+		return false;
+	}
+
+	APawn* MyPawn = GetPawn();
+	if (!MyPawn)
+	{
+		return false;
+	}
+
+	ALRCharacter* OwnerCharacter = Cast<ALRCharacter>(MyPawn);
+	if (!OwnerCharacter)
+	{
+		return false;
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = OwnerCharacter;
+	EventData.Target = Target;
+
+	// TODO: 추후 데이터 테이블에서 각각 태그 가져와서 스킬 달리 사용하도록 로직 리팩토링
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+		OwnerCharacter,
+		LRTags::Ability_Combat_BasicShoot,
+		EventData);
+
+	return true;
 }
