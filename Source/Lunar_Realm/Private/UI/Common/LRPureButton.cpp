9353d8387390b7b9200e0c41c353de90c1065996
jbaem <101052774+jbaem@users.noreply.github.com>
2026-03-11 15:18:49 +0900

    <feat> 투명 버튼 생성

---
+++ Source/Lunar_Realm/Private/UI/Common/LRPureButton.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Common/LRPureButton.h"
+
+ULRPureButton::ULRPureButton()
+{
+	ApplyTransparentStyle();
+}
+
+void ULRPureButton::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+	ApplyTransparentStyle();
+}
+
+void ULRPureButton::ApplyTransparentStyle()
+{
+	FButtonStyle TransparentStyle = WidgetStyle;
+
+	FSlateBrush TransparentBrush;
+	TransparentBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
+	TransparentBrush.TintColor = FSlateColor(FLinearColor(0.f, 0.f, 0.f, 0.f));
+
+	TransparentStyle.SetNormal(TransparentBrush);
+	TransparentStyle.SetHovered(TransparentBrush);
+	TransparentStyle.SetPressed(TransparentBrush);
+
+	SetStyle(TransparentStyle);
+	BackgroundColor = FLinearColor(0.f, 0.f, 0.f, 0.f);
+}
---
+++ Source/Lunar_Realm/Public/UI/Common/LRPureButton.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "LRPureButton.generated.h"
+
+/**
+ * 배경이 투명하고 클릭 영역만 존재하는 투명 버튼 클래스
+ */
+UCLASS()
+class LUNAR_REALM_API ULRPureButton : public UButton
+{
+	GENERATED_BODY()
+	
+public:
+	ULRPureButton();
+
+protected:
+	// 에디터에서 수정 후 실행 시 스타일이 초기화되는 것을 방지하기 위함.
+	virtual void SynchronizeProperties() override;
+
+private:
+	void ApplyTransparentStyle();
+};
