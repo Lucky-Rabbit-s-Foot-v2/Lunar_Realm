7dfe502a84d4566fd43eaad1f62b529b0ec16847
jbaem <101052774+jbaem@users.noreply.github.com>
2026-03-18 11:26:26 +0900

    <feat> 기본 버튼 추가
    - 이미지와 텍스트를 포함하는 기본 버튼 추가
    - UI 닫기
    - UI 열기
    - Level 이동

---
+++ Source/Lunar_Realm/Public/UI/Common/LRCloseUIButton.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Core/LRButtonWidget.h"
+#include "LRCloseUIButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRCloseUIButton : public ULRButtonWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetTargetWidget(ULRBaseWidget* InTargetWidget) { TargetWidget = InTargetWidget; }
+	
+	virtual void OnButtonClicked() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LR|UI|Close Button")
+	class ULRBaseWidget* TargetWidget = nullptr;
+};
