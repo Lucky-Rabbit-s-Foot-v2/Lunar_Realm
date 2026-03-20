f797ef0594255c84a5e82f081cf3a0bc907e5cb6
jbaem <101052774+jbaem@users.noreply.github.com>
2026-03-20 17:02:10 +0900

    <design> 상점 디자인 수정

--- Source/Lunar_Realm/Private/UI/Shop/LRShopPageWidget.cpp
+++ Source/Lunar_Realm/Private/UI/Shop/LRShopPageWidget.cpp
@@ -2,26 +2,3 @@
 
 
 #include "UI/Shop/LRShopPageWidget.h"
-
-#include "Components/Button.h"
-#include "Units/LRControllerBase.h"
-
-void ULRShopPageWidget::BindProperties()
-{
-	Super::BindProperties();
-
-	if (Btn_Close) Btn_Close->OnClicked.AddDynamic(this, &ULRShopPageWidget::CloseButtonClicked);
-}
-
-void ULRShopPageWidget::UnbindProperties()
-{
-	if (Btn_Close) Btn_Close->OnClicked.Clear();
-
-	Super::UnbindProperties();
-}
-
-void ULRShopPageWidget::CloseButtonClicked()
-{
-	UUIManagerSubsystem* UIManager = GetGameInstance()->GetSubsystem<UUIManagerSubsystem>();
-	UIManager->OpenUIByID(EUIID::LOBBY);
-}
