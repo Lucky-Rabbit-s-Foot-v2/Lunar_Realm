94db414e56c50b176f57750a358ea2c413f00e3c
jackbongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-22 15:20:25 +0900

    <feat> Demonia 전용기 생성

---
+++ Source/Lunar_Realm/Public/GAS/Ability/Skill/PlayerSkill/LRGA_DemoniaSkill.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/Skill/LRGA_Fireball.h"
+#include "LRGA_DemoniaSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRGA_DemoniaSkill : public ULRGA_Fireball
+{
+	GENERATED_BODY()
+
+public:
+	ULRGA_DemoniaSkill();
+	
+};
