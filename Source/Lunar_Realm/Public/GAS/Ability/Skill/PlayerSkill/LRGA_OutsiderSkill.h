44ec69e630108ddc20121942630591e9204aa81d
jackbongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-22 18:09:33 +0900

    <feat> Outsider 전용기 생성

---
+++ Source/Lunar_Realm/Public/GAS/Ability/Skill/PlayerSkill/LRGA_OutsiderSkill.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/Skill/LRGA_LightningStrike.h"
+#include "LRGA_OutsiderSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRGA_OutsiderSkill : public ULRGA_LightningStrike
+{
+	GENERATED_BODY()
+
+public:
+	ULRGA_OutsiderSkill();
+	
+};
