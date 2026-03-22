cb1f2ce61894e2a66445ecd6e1ef60aa699e974b
jackbongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-22 18:21:06 +0900

    <feat> Pioneer 전용기 생성

---
+++ Source/Lunar_Realm/Public/GAS/Ability/Skill/PlayerSkill/LRGA_PioneerSkill.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/Skill/PlayerSkill/LRGA_ArmySkill.h"
+#include "LRGA_PioneerSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRGA_PioneerSkill : public ULRGA_ArmySkill
+{
+	GENERATED_BODY()
+	
+
+public:
+	ULRGA_PioneerSkill();
+
+protected:
+	virtual void OnAbilityActivated(const FGameplayAbilitySpecHandle InHandle, const FGameplayAbilityActorInfo* InActorInfo, const FGameplayAbilityActivationInfo InActivationInfo) override;
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle InHandle, const FGameplayAbilityActorInfo* InActorInfo, const FGameplayAbilityActivationInfo InActivationInfo, bool bInReplicateEndAbility, bool bInWasCancelled) override;
+
+private:
+	FTimerHandle SecondStrikeTimer;
+	FTimerHandle ThirdStrikeTimer;
+};
