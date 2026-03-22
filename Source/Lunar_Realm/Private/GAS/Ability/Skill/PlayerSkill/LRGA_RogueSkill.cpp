85d0d91452fd66b6fe60cd0d9ef950ef312251a4
jackbongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-22 18:47:47 +0900

    <feat> Rogue 전용기 생성

---
+++ Source/Lunar_Realm/Private/GAS/Ability/Skill/PlayerSkill/LRGA_RogueSkill.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Ability/Skill/PlayerSkill/LRGA_RogueSkill.h"
+#include "GAS/Tags/LRGameplayTags.h"
+
+ULRGA_RogueSkill::ULRGA_RogueSkill()
+{
+
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+	AbilityTriggers.Empty();
+	FAbilityTriggerData TriggerData;
+	TriggerData.TriggerTag = LRTags::Ability_Skill_Rogue;
+	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
+	AbilityTriggers.Add(TriggerData);
+
+	SkillID = "SKILL_ROGUE";
+	SkillEffectID = "EFFECT_ROGUE";
+
+	CooldownTagContainer.Reset();
+	CooldownTagContainer.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Skill.Rogue.Cooldown")));
+}
+
