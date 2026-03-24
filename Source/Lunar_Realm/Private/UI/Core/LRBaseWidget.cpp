5c23a64bbaee8d13d72ef9267a31eda21faafbd4
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-24 14:20:55 +0900

    <fix> 스테이지 클리어시 터지는 버그 픽스

--- Source/Lunar_Realm/Private/UI/Core/LRBaseWidget.cpp
+++ Source/Lunar_Realm/Private/UI/Core/LRBaseWidget.cpp
@@ -52,14 +52,6 @@ void ULRBaseWidget::InitializeUI()
 void ULRBaseWidget::DeinitializeUI()
 {
 	// 자식 클래스에서 오버라이드하여 정리 로직 구현
-	for (int32 i = SubWidgets.Num() - 1; i >= 0; --i)
-	{
-		if (IsValid(SubWidgets[i]))
-		{
-			SubWidgets[i]->DeinitializeUI();
-		}
-	}
-	SubWidgets.Empty();
 }
 
 void ULRBaseWidget::RegisterSubWidgets()
