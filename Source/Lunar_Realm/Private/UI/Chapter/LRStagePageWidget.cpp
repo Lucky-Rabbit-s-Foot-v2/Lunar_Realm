6b5876f51f03e4da9954e3a5bfc4f872053177cd
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-25 17:18:33 +0900

    <feat> 스테이지 전환 애니메이션 적용

--- Source/Lunar_Realm/Private/UI/Chapter/LRStagePageWidget.cpp
+++ Source/Lunar_Realm/Private/UI/Chapter/LRStagePageWidget.cpp
@@ -14,6 +14,10 @@
 #include "Engine/GameInstance.h"
 #include "Subsystems/UIManagerSubsystem.h"
 #include "Subsystems/GameDataSubsystem.h"
+#include "Subsystems/StageManagerSubsystem.h"
+
+#include "TimerManager.h"
+#include "Animation/WidgetAnimation.h"
 
 void ULRStagePageWidget::InitializeUI()
 {
@@ -44,6 +48,13 @@ void ULRStagePageWidget::RegisterSubWidgets()
 	SubWidgets.Add(Stage5);
 }
 
+void ULRStagePageWidget::OpenUI()
+{
+	Super::OpenUI();
+
+	PlayAnimation(Anim_FadeIn);
+}
+
 void ULRStagePageWidget::SetChapterID(FName InID)
 {
 	CurrentChapterID = InID;
@@ -71,9 +82,70 @@ void ULRStagePageWidget::SetStageData(const TArray<FName>& StageIDs)
 			StageWidget->SetStageID(StageIDs[i]);
 		}
 	}
+
+	UpdatePaths(StageIDs);
 }
 
 void ULRStagePageWidget::OnChapterSelectionChanged(FString SelectedItem, ESelectInfo::Type SelectionType)
 {
-	SetChapterID(FName(SelectedItem));
+	if (SelectionType == ESelectInfo::Direct || SelectedItem == CurrentChapterID.ToString())
+	{
+		return;
+	}
+
+	if (Anim_FadeOut)
+	{
+		PlayAnimation(Anim_FadeOut);
+
+		float FadeOutTime = Anim_FadeOut->GetEndTime(); + 0.15f;
+
+		GetWorld()->GetTimerManager().SetTimer(
+			ChapterChangeTimer,
+			[this, SelectedItem]()
+			{
+				SetChapterID(FName(SelectedItem));
+
+				if (Anim_FadeIn)
+				{
+					PlayAnimation(Anim_FadeIn);
+				}
+			},
+			FadeOutTime,
+			false
+		);
+	}
+	else
+	{
+		SetChapterID(FName(SelectedItem));
+	}
+}
+
+void ULRStagePageWidget::UpdatePaths(const TArray<FName>& InStageIDs)
+{
+	if (UStageManagerSubsystem* StageMgr = GetGameInstance()->GetSubsystem<UStageManagerSubsystem>())
+	{
+		// 1->2 가는 길 
+		if (Img_Path1To2 && InStageIDs.IsValidIndex(1))
+		{
+			const FStageClearedData& ClearedData = StageMgr->GetStageClearedData(InStageIDs[1]);
+			bool bIsUnlocked = ClearedData.bIsUnlocked || StageMgr->IsCheatStageUnlocked();
+			Img_Path1To2->SetColorAndOpacity(bIsUnlocked ? FLinearColor::White : FLinearColor(0.1f, 0.1f, 0.1f, 1.0f));
+		}
+
+		// 2->3 가는 길
+		if (Img_Path2To3 && InStageIDs.IsValidIndex(2))
+		{
+			const FStageClearedData& ClearedData = StageMgr->GetStageClearedData(InStageIDs[2]);
+			bool bIsUnlocked = ClearedData.bIsUnlocked || StageMgr->IsCheatStageUnlocked();
+			Img_Path2To3->SetColorAndOpacity(bIsUnlocked ? FLinearColor::White : FLinearColor(0.1f, 0.1f, 0.1f, 1.0f));
+		}
+
+		// 3->4 가는 길
+		if (Img_Path3To4 && InStageIDs.IsValidIndex(3))
+		{
+			const FStageClearedData& ClearedData = StageMgr->GetStageClearedData(InStageIDs[3]);
+			bool bIsUnlocked = ClearedData.bIsUnlocked || StageMgr->IsCheatStageUnlocked();
+			Img_Path3To4->SetColorAndOpacity(bIsUnlocked ? FLinearColor::White : FLinearColor(0.1f, 0.1f, 0.1f, 1.0f));
+		}
+	}
 }
