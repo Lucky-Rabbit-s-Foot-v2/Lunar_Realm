d59471d269c44c3b72200c38f074fa3c3aae2340
jbaem <101052774+jbaem@users.noreply.github.com>
2026-03-27 16:58:15 +0900

    <feat> 스테이지 준비창 파티 라인업 데이터 입력 수정

---
+++ Source/Lunar_Realm/Private/UI/Chapter/LRReadySlot.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Chapter/LRReadySlot.h"
+
+#include "Components/Image.h"
+
+void ULRReadySlot::SetData(UTexture2D* InPortrait, UTexture2D* InGrade, UTexture2D* InEquip)
+{
+	if (Portrait)
+	{
+		Portrait->SetBrushFromTexture(InPortrait);
+	}
+	
+	if (Grade)
+	{
+		Grade->SetVisibility(InGrade ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+		Grade->SetBrushFromTexture(InGrade);
+	}
+
+	if (Equip)
+	{
+		Equip->SetBrushFromTexture(InEquip);
+	}
+}
