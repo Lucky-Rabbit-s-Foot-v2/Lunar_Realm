d527ab60f7725966fdfbd9b606cd9de0ca116498
JackBongmin <229694458+jackbongmin@users.noreply.github.com>
2026-03-27 13:21:04 +0900

    <feat> GA_Heal 로직 수정

--- Source/Lunar_Realm/Private/GAS/Ability/Player/LRGA_Heal.cpp
+++ Source/Lunar_Realm/Private/GAS/Ability/Player/LRGA_Heal.cpp
@@ -5,49 +5,97 @@
 #include "AbilitySystemComponent.h"
 #include "GAS/Attributes/LRPlayerAttributeSet.h"
 #include "GAS/Tags/LRGameplayTags.h"
+#include "Subsystems/GameDataSubsystem.h"
+#include "Engine/GameInstance.h"
 
 
 ULRGA_Heal::ULRGA_Heal()
 {
-	//AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Skill.Heal")));
-	// FGameplayTagContainer Tags;
-	// Tags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Skill.Heal")));
-	// SetAssetTags(Tags);
+
+
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
-	
-	FGameplayTagContainer TempTags = GetAssetTags();
-	TempTags.AddTag(LRTags::Ability_Skill_Heal);
-	SetAssetTags(TempTags);
-	
-	//(260219) KHS 이벤트 태그를 전달하여 발동되도록 트리거 등록
+
 	FAbilityTriggerData TriggerData;
 	TriggerData.TriggerTag = LRTags::Ability_Skill_Heal;
 	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
 	AbilityTriggers.Add(TriggerData);
+
+	FGameplayTagContainer TempTags;
+	TempTags.AddTag(LRTags::Ability_Skill_Heal);
+	AbilityTags = TempTags;
+
+	ActivationBlockedTags.AddTag(LRTags::State_Dead);
+
+
+	//InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	//
+	//FGameplayTagContainer TempTags = GetAssetTags();
+	//TempTags.AddTag(LRTags::Ability_Skill_Heal);
+	//SetAssetTags(TempTags);
+	//
+	////(260219) KHS 이벤트 태그를 전달하여 발동되도록 트리거 등록
+	//FAbilityTriggerData TriggerData;
+	//TriggerData.TriggerTag = LRTags::Ability_Skill_Heal;
+	//TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
+	//AbilityTriggers.Add(TriggerData);
+
+	//SkillID = "SKILL_PLAYERHEAL";
+	//SkillEffectID = "EFFECT_PLAYERHEAL";
+
+	//CooldownTagContainer.Reset();
+	//ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Cooldown.Skill.Heal")));
+
+
+
+
+
 }
 
-void ULRGA_Heal::OnAbilityActivated(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo)
+//void ULRGA_Heal::OnAbilityActivated(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+//	const FGameplayAbilityActivationInfo ActivationInfo)
+//{
+//	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+//	{
+//		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+//		return;
+//	}
+//
+//	if (HealEffectClass && ActorInfo->AbilitySystemComponent.IsValid())
+//	{
+//		FGameplayEffectContextHandle ContextHandle = ActorInfo->AbilitySystemComponent->MakeEffectContext();
+//		FGameplayEffectSpecHandle SpecHandle = ActorInfo->AbilitySystemComponent->MakeOutgoingSpec(HealEffectClass, 1.0f, ContextHandle);
+//
+//		if (SpecHandle.IsValid())
+//		{
+//			ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+//			UE_LOG(LogTemp, Warning, TEXT("체력 회복 성공!"));
+//		}
+//	}
+//
+//	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+//}
+
+void ULRGA_Heal::ActivateAbility(const FGameplayAbilitySpecHandle InHandle, const FGameplayAbilityActorInfo* InActorInfo, const FGameplayAbilityActivationInfo InActivationInfo, const FGameplayEventData* InTriggerEventData)
 {
-	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	if (!CommitAbility(InHandle, InActorInfo, InActivationInfo))
 	{
-		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		EndAbility(InHandle, InActorInfo, InActivationInfo, true, true);
 		return;
 	}
 
-	if (HealEffectClass && ActorInfo->AbilitySystemComponent.IsValid())
+	if (HealEffectClass && InActorInfo->AbilitySystemComponent.IsValid())
 	{
-		FGameplayEffectContextHandle ContextHandle = ActorInfo->AbilitySystemComponent->MakeEffectContext();
-		FGameplayEffectSpecHandle SpecHandle = ActorInfo->AbilitySystemComponent->MakeOutgoingSpec(HealEffectClass, 1.0f, ContextHandle);
+		FGameplayEffectContextHandle ContextHandle = InActorInfo->AbilitySystemComponent->MakeEffectContext();
+		FGameplayEffectSpecHandle SpecHandle = InActorInfo->AbilitySystemComponent->MakeOutgoingSpec(HealEffectClass, 1.0f, ContextHandle);
 
 		if (SpecHandle.IsValid())
 		{
-			ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+			InActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
 			UE_LOG(LogTemp, Warning, TEXT("체력 회복 성공!"));
 		}
 	}
 
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	EndAbility(InHandle, InActorInfo, InActivationInfo, true, false);
 }
 
 bool ULRGA_Heal::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
@@ -67,20 +115,44 @@ bool ULRGA_Heal::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, con
 	if (CurrentHealth >= MaxHealth)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("체력이 이미 가득 참"));
-		return false; 
+		return false;
 	}
 
 	return true;
 }
 
-const FGameplayTagContainer* ULRGA_Heal::GetCooldownTags() const
-{
-	static FGameplayTagContainer CooldownTags;
-
-	if (CooldownTags.IsEmpty())
-	{
-		CooldownTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Cooldown.Skill.Heal")));
-	}
-
-	return &CooldownTags;
-}
+//const FGameplayTagContainer* ULRGA_Heal::GetCooldownTags() const
+//{
+//	static FGameplayTagContainer CooldownTags;
+//
+//	if (CooldownTags.IsEmpty())
+//	{
+//		CooldownTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Cooldown.Skill.Heal")));
+//	}
+//
+//	return &CooldownTags;
+//}
+//
+//void ULRGA_Heal::ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+//{
+//	UGameplayEffect* CostGE = GetCostGameplayEffect();
+//	if (!CostGE)
+//	{
+//		return;
+//	}
+//
+//	FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(Handle, ActorInfo, ActivationInfo, CostGameplayEffectClass);
+//
+//	if (UGameDataSubsystem* DataSys = GetWorld()->GetGameInstance()->GetSubsystem<UGameDataSubsystem>())
+//	{
+//		const FSkillEffectData& EffectData = DataSys->GetSkillEffectData(FName(SkillEffectID));
+//
+//		if (EffectData.Cost > 0.f)
+//		{
+//			SpecHandle.Data->SetByCallerTagMagnitudes.Add(LRTags::Data_Cost, EffectData.Cost);
+//		}
+//	}
+//
+//
+//	ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, SpecHandle);
+//}
