6da4ef012c5be07044c4e1e1b3759200621c5f89
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2026-03-30 00:38:45 +0900

    <feat> 도감 목록은 Entry Widget, 파티 창은 Slot Widget 으로 인터페이스화

---
+++ Source/Lunar_Realm/Private/UI/Core/LRSlotWidget.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Core/LRSlotWidget.h"
+
+#include "Components/Button.h"
+#include "Components/Image.h"
+
+#include "Engine/GameInstance.h"
+#include "Engine/Texture2D.h"
+
+#include "Subsystems/SaveGameSubsystem.h"
+#include "Subsystems/GameDataSubsystem.h"
+#include "Subsystems/CollectionSubsystem.h"
+
+#include "Units/OutGame/LROutGameController.h"
+
+
+void ULRSlotWidget::BindProperties()
+{
+	Super::BindProperties();
+
+	Button->OnClicked.AddUniqueDynamic(this, &ULRSlotWidget::OnSlotClicked);
+}
+
+void ULRSlotWidget::UnbindProperties()
+{
+	Button->OnClicked.Clear();
+	OnSlotClickedDel.Clear();
+	
+	Super::UnbindProperties();
+}
+
+void ULRSlotWidget::RefreshUI()
+{
+	Super::RefreshUI();
+
+	SetGradeImage();
+	SetIconImage();
+	SetSelected(bIsSelected);
+}
+
+void ULRSlotWidget::BindToController(ALRControllerBase* Controller)
+{
+	Super::BindToController(Controller);
+
+	ALROutGameController* PC = Cast<ALROutGameController>(Controller);
+	if (PC)
+	{
+		OnSlotClickedDel.AddUniqueDynamic(PC, &ALROutGameController::OnSelectedSlotWidget);
+		OnSlotToggledDel.AddUniqueDynamic(PC, &ALROutGameController::OnSelectedSlotToggled);
+	}
+}
+
+void ULRSlotWidget::SetSlotIndex(int32 InIndex)
+{
+	SlotIndex = InIndex;
+}
+
+void ULRSlotWidget::SetID(FName InID)
+{
+	ID = InID;
+
+	RefreshUI();
+}
+
+void ULRSlotWidget::OnSlotClicked()
+{
+	SetSelected(true);
+	OnSlotClickedDel.Broadcast(this);
+}
+
+void ULRSlotWidget::SetGradeImage()
+{
+	if (ID.IsNone())
+	{
+		Img_Grade->SetVisibility(ESlateVisibility::Hidden);
+		return;
+	}
+}
+
+void ULRSlotWidget::SetIconImage()
+{
+	if (ID.IsNone())
+	{
+		Image->SetBrushFromTexture(EmptySlotTexture);
+		return;
+	}
+}
+
+void ULRSlotWidget::SetSelected(bool bSelected)
+{
+	bIsSelected = bSelected;
+
+	if (bIsSelected)
+	{
+		Img_Selected->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Img_Selected->SetVisibility(ESlateVisibility::Hidden);
+	}
+
+	OnSlotToggledDel.Broadcast(bIsSelected);
+}
---
+++ Source/Lunar_Realm/Public/UI/Core/LRSlotWidget.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Core/LRButtonWidget.h"
+#include "Data/LRDataStructs.h"
+#include "Data/LREnumType.h"
+#include "LRSlotWidget.generated.h"
+
+/**
+ * 
+ */
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSlotClicked, ULRSlotWidget*, InWidget);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSlotToggled, bool, InIsSelected);
+
+UCLASS()
+class LUNAR_REALM_API ULRSlotWidget : public ULRButtonWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BindProperties() override;
+	virtual void UnbindProperties() override;
+
+	virtual void RefreshUI() override;
+
+	virtual void BindToController(class ALRControllerBase* Controller) override;
+
+	virtual void SetSlotIndex(int32 InIndex);
+	virtual void SetID(FName InID);
+	virtual void SetGradeImage();
+	virtual void SetIconImage();
+
+	virtual void SetSelected(bool bSelected);
+
+	int32 GetSlotIndex() const { return SlotIndex; }
+	ECollectionType GetType() const { return Type; }
+	FName GetID() const { return ID; }
+
+	UFUNCTION()
+	void OnSlotClicked();
+	UPROPERTY(BlueprintAssignable, Category = "LR|UI|Events")
+	FOnSlotClicked OnSlotClickedDel;
+
+	UPROPERTY(BlueprintAssignable, Category = "LR|UI|Events")
+	FOnSlotToggled OnSlotToggledDel;
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> Img_Grade;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> Img_Selected;
+
+	UPROPERTY(EditAnywhere, Category = "LR|UI Slot")
+	int32 SlotIndex = 0;
+
+	UPROPERTY(EditAnywhere, Category = "LR|UI|Empty")
+	TObjectPtr<class UTexture2D> EmptySlotTexture;
+
+	bool bIsSelected = false;
+	FName ID;
+	ECollectionType Type = ECollectionType::NONE;
+};
