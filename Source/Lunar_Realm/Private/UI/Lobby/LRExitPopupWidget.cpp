1f8d7bac07dbb0d79b34e79995d80f6ef7c11ba0
jbaem <101052774+jbaem@users.noreply.github.com>
2026-03-31 09:59:16 +0900

    <feat> 게임 종료 버튼 추가

---
+++ Source/Lunar_Realm/Private/UI/Lobby/LRExitPopupWidget.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Lobby/LRExitPopupWidget.h"
+
+#include "Engine/GameInstance.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+#include "Subsystems/UIManagerSubsystem.h"
+
+#include "UI/Core/LRButtonWidget.h"
+
+void ULRExitPopupWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (UUIManagerSubsystem* UIManager = GetGameInstance()->GetSubsystem<UUIManagerSubsystem>())
+	{
+		OnCloseUIRequestedDel.AddUniqueDynamic(UIManager, &UUIManagerSubsystem::CloseUI);
+	}
+}
+
+void ULRExitPopupWidget::BindProperties()
+{
+	Super::BindProperties();
+
+	Btn_Exit->OnLRButtonClickedDel.AddUniqueDynamic(this, &ULRExitPopupWidget::OnExitButtonClicked);
+	Btn_Back->OnLRButtonClickedDel.AddUniqueDynamic(this, &ULRExitPopupWidget::OnBackButtonClicked);
+}
+
+void ULRExitPopupWidget::UnbindProperties()
+{
+	Btn_Back->OnLRButtonClickedDel.Clear();
+	Btn_Exit->OnLRButtonClickedDel.Clear();
+
+	Super::UnbindProperties();
+}
+
+void ULRExitPopupWidget::OnExitButtonClicked()
+{
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	APlayerController* PC = World->GetFirstPlayerController();
+	UKismetSystemLibrary::QuitGame(World, PC, EQuitPreference::Quit, true);
+}
+
+void ULRExitPopupWidget::OnBackButtonClicked()
+{
+	OnCloseUIRequestedDel.Broadcast(this);
+}
---
+++ Source/Lunar_Realm/Public/UI/Lobby/LRExitPopupWidget.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Core/LRPopupWidget.h"
+#include "LRExitPopupWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRExitPopupWidget : public ULRPopupWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+
+	virtual void BindProperties() override;
+	virtual void UnbindProperties() override;
+
+
+	UFUNCTION()
+	void OnExitButtonClicked();
+
+	UFUNCTION()
+	void OnBackButtonClicked();
+
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class ULRButtonWidget> Btn_Exit;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class ULRButtonWidget> Btn_Back;
+
+};
