f41fd37cb1f6111297ce8780524e3ab0e78f4a52
jbaem <101052774+jbaem@users.noreply.github.com>
2026-04-01 13:45:50 +0900

    <feat> 보스 등장시 알림 팝업 위젯 생성

---
+++ Source/Lunar_Realm/Public/UI/InGame/LRBossAlertPopupWidget.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Core/LRPopupWidget.h"
+#include "LRBossAlertPopupWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNAR_REALM_API ULRBossAlertPopupWidget : public ULRPopupWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	virtual void OpenUI() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void OnPlayAlertAnimation();
+	
+	virtual void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
+
+protected:
+	UPROPERTY(meta = (BindWidgetAnim), Transient)
+	class UWidgetAnimation* AlertAnim;
+
+};
